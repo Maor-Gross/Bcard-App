4cf79d1dfa9cef0b1e0b8cfceafae1b06c0857d6
Maor-Gross <maorgross247@gmail.com>
2025-03-29 20:12:18 +0300

    first commit

---
+++ src/components/DarkMode.tsx
@@ -0,0 +1,42 @@
+import { FunctionComponent, useEffect, useState } from "react";
+
+interface DarkModeProps {}
+
+const DarkMode: FunctionComponent<DarkModeProps> = () => {
+  const [darkMode, setDarkMode] = useState(() => {
+    const storedDarkMode = localStorage.getItem("darkMode");
+    return storedDarkMode === "true";
+  });
+
+  useEffect(() => {
+    const body = document.querySelector("body");
+    const footer = document.querySelector("footer");
+
+    if (body) {
+      body.setAttribute("data-bs-theme", darkMode ? "dark" : "light");
+    }
+    if (footer) {
+      footer.style.backgroundColor = darkMode ? "black" : "white";
+    }
+
+    localStorage.setItem("darkMode", String(darkMode));
+  }, [darkMode]);
+
+  const changeMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return darkMode ? (
+    <i
+      className="bi bi-sun-fill text-warning light-mode"
+      style={{ cursor: "pointer" }}
+      onClick={changeMode}></i>
+  ) : (
+    <i
+      className="bi bi-moon-fill text-light dark-mode"
+      style={{ cursor: "pointer" }}
+      onClick={changeMode}></i>
+  );
+};
+
+export default DarkMode;
